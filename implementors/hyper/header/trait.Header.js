350529542edfa06e05c07aec6fc1bcfb68246bc4
Mike Dilger <mike@efx.co.nz>
2015-06-15 09:02:49 +1200

    Update documentation

--- implementors/hyper/header/trait.Header.js
+++ implementors/hyper/header/trait.Header.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['hyper'] = [];implementors['formdata'] = ["impl <a class='trait' href='hyper/header/trait.Header.html' title='hyper::header::Header'>Header</a> for <a class='struct' href='formdata/struct.ContentDispositionFormData.html' title='formdata::ContentDispositionFormData'>ContentDispositionFormData</a>",];
+implementors['formdata'] = ["impl <a class='trait' href='https://hyperium.github.io/hyper/hyper/index.html/hyper/header/trait.Header.html' title='hyper::header::Header'>Header</a> for <a class='struct' href='formdata/struct.ContentDispositionFormData.html' title='formdata::ContentDispositionFormData'>ContentDispositionFormData</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
