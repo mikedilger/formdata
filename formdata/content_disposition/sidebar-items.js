e0baf90d2de7a02bd444e9e891108eb11194c6d6
Mike Dilger <mike@efx.co.nz>
2015-07-08 08:20:16 +1200

    Updated docs

--- formdata/content_disposition/sidebar-items.js
+++ formdata/content_disposition/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"struct":[["ContentDispositionFormData","We define a Content-Disposition form-data only header, which is found within the mime multipart sections.  NOT FOR GENERAL USE"]]});
+initSidebarItems({"struct":[["ContentDispositionFormData","We define a Content-Disposition form-data only header, which is found within the mime multipart sections.  NOT FOR GENERAL USE, as it is very limited in scope."]]});
