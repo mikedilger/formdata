ecfc8b92945f1489e6fab7dd89dffeb01d794afc
Mike Dilger <mike@mikedilger.com>
2015-07-09 03:55:12 +1200

    doc update

--- formdata/content_disposition/sidebar-items.js
+++
@@ -1,1 +0,0 @@
-initSidebarItems({"struct":[["ContentDispositionFormData","We define a Content-Disposition form-data only header, which is found within the mime multipart sections.  NOT FOR GENERAL USE, as it is very limited in scope."]]});
