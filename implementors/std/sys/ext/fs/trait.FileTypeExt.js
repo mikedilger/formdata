96072da5b7a2d1ab6b7a1a1baa2b044eb9cd6763
Mike Dilger <mike@efx.co.nz>
2015-08-07 09:44:47 +1200

    Doc Update

--- implementors/std/sys/ext/fs/trait.FileTypeExt.js
+++ implementors/std/sys/ext/fs/trait.FileTypeExt.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['formdata'] = [];
+implementors['openssl'] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
