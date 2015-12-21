43267288746db83ceabec6ff64cdd65abd21d297
Mike Dilger <mike@efx.co.nz>
2015-12-22 09:30:33 +1300

    Use hyper's ContentDisposition header:
    Does not handle encodings for the filename parameter, see FIXME in the code

--- src/lib.rs
+++ src/lib.rs
@@ -20,19 +20,17 @@ extern crate serde_json;
 
 pub mod buf;
 pub mod error;
-mod headers;
 #[cfg(test)]
 mod mock;
 
 use std::fs::File;
 use std::io::{BufRead, BufReader, Read};
 
-use hyper::header::{ContentType, Headers};
+use hyper::header::{ContentType, Headers, ContentDisposition, DispositionType, DispositionParam};
 use mime::{Attr, Mime, Param, SubLevel, TopLevel, Value};
 
 use buf::BufReadExt;
 pub use error::Error;
-use headers::ContentDisposition;
 
 pub mod uploaded_file;
 pub use uploaded_file::UploadedFile;
@@ -131,20 +129,23 @@ fn run_state_machine<R: BufRead>(boundary: String, reader: &mut R, form_data: &m
                             Some(cd) => cd,
                             None => return Err(Error::MissingDisposition),
                         };
+                        let cd_filename: Option<String> = get_content_disposition_filename(cd);
 
                         let ct: Option<&ContentType> = headers.get();
 
                         match mode {
-                            FormData if cd.disposition == "form-data" => {
+                            FormData if cd.disposition ==
+                                DispositionType::Ext("form-data".to_owned()) => {
                                 state = if is_multipart_mixed(ct) {
                                     CapturingMixed(cd.clone(), ct.unwrap().clone())
-                                } else if ct.is_some() || cd.filename.is_some() {
+                                } else if ct.is_some() || cd_filename.is_some() {
                                     CapturingFile(cd.clone(), ct.map(|ct| ct.clone()))
                                 } else {
                                     CapturingValue(cd.clone())
                                 };
                             },
-                            Mixed(_) if cd.disposition == "file" => {
+                            Mixed(_) if cd.disposition == DispositionType::Ext("file".to_owned())
+                                || cd.disposition == DispositionType::Attachment => {
                                 state = CapturingFile(cd.clone(), ct.map(|ct| ct.clone()));
                             },
                             _ => return Err(Error::InvalidDisposition),
@@ -155,17 +156,19 @@ fn run_state_machine<R: BufRead>(boundary: String, reader: &mut R, form_data: &m
                 }
             },
             CapturingMixed(cd, ct) => {
+                let cd_name: Option<String> = get_content_disposition_name(&cd);
                 let boundary = try!(get_boundary_token(&(ct.0).2));
-                let mode = Mixed(try!(cd.name.ok_or(Error::NoName)));
+                let mode = Mixed(try!(cd_name.ok_or(Error::NoName)));
                 try!(run_state_machine(boundary, reader, form_data, mode));
                 state = Discarding;
             },
             CapturingValue(_) if mode != FormData => unreachable!(),
             CapturingValue(cd) => {
+                let cd_name: Option<String> = get_content_disposition_name(&cd);
                 buf.truncate(0);
                 let _ = try!(reader.stream_until_token(&crlf_boundary, &mut buf));
 
-                let key = try!(cd.name.ok_or(Error::NoName));
+                let key = try!(cd_name.ok_or(Error::NoName));
                 let val = try!(String::from_utf8(buf));
 
                 form_data.fields.push((key, val));
@@ -173,21 +176,26 @@ fn run_state_machine<R: BufRead>(boundary: String, reader: &mut R, form_data: &m
                 state = ReadingHeaders;
             },
             CapturingFile(cd, ct) => {
+                let cd_name: Option<String> = get_content_disposition_name(&cd);
+                let cd_filename: Option<String> = get_content_disposition_filename(&cd);
+
                 // Setup a file to capture the contents.
                 let mut uploaded_file = try!(UploadedFile::new(
                     ct.map_or(mime!(Text/Plain; Charset=Utf8), |ct| ct.0)));
-                uploaded_file.filename = cd.filename.clone();
+                uploaded_file.filename = cd_filename.clone();
                 let mut file = try!(File::create(uploaded_file.path.clone()));
 
                 // Stream out the file.
                 let read = try!(reader.stream_until_token(&crlf_boundary, &mut file));
                 uploaded_file.size = read - crlf_boundary.len();
 
-                // TODO: Handle Content-Transfer-Encoding.
+                // TODO: Handle Content-Transfer-Encoding.  RFC 7578 section 4.7 deprecated
+                // this, and the authors state "Currently, no deployed implementations that
+                // send such bodies have been discovered", so this is very low priority.
 
                 let key = match mode {
                     Mixed(ref name) => name.clone(),
-                    FormData => try!(cd.name.ok_or(Error::NoName)),
+                    FormData => try!(cd_name.ok_or(Error::NoName)),
                 };
 
                 form_data.files.push((key, uploaded_file));
@@ -235,6 +243,35 @@ pub fn get_multipart_boundary(headers: &Headers) -> Result<String, Error> {
     }
 
     get_boundary_token(params)
+}
+
+#[inline]
+fn get_content_disposition_name(cd: &ContentDisposition) -> Option<String> {
+    if let Some(&DispositionParam::Ext(_, ref value)) = cd.parameters.iter()
+        .find(|&x| match *x {
+            DispositionParam::Ext(ref token,_) => &*token == "name",
+            _ => false,
+        })
+    {
+        Some(value.clone())
+    } else {
+        None
+    }
+}
+
+#[inline]
+fn get_content_disposition_filename(cd: &ContentDisposition) -> Option<String> {
+    if let Some(&DispositionParam::Filename(ref _charset, _, ref bytes)) =
+        cd.parameters.iter().find(|&x| match *x {
+            DispositionParam::Filename(_,_,_) => true,
+            _ => false,
+        })
+    {
+        // FIXME: Translate bytes from _charset to utf-8
+        Some(String::from_utf8_lossy(&*bytes).into_owned())
+    } else {
+        None
+    }
 }
 
 fn get_boundary_token(params: &[Param]) -> Result<String, Error> {
